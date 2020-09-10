917c85149920f00d72056f7678c931bac249629d
yongju <yongjujiwork07@gmail.com>
2020-09-10 18:26:02 +0900

    create actionを実装&readmeの編集

--- app/controllers/receptions_controller.rb
+++ app/controllers/receptions_controller.rb
@@ -1,4 +1,20 @@
 class ReceptionsController < ApplicationController
   def new
+    @reception = Reception.new
   end
+
+  def create
+    @reception = Reception.new(reception_params)
+    if @reception.save
+      render 'new'
+    else
+      render 'new'
+    end
+  end
+
+  private
+  
+    def reception_params
+      params.require(:reception).permit(:name, :purpose, :organization)
+    end
 end
