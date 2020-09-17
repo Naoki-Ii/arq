f7735c0af8920f4a1821297c425f2ffe3741b9b1
yongju <yongjujiwork07@gmail.com>
2020-09-17 17:07:05 +0900

    テストを修正

--- spec/features/reception_spec.rb
+++ spec/features/reception_spec.rb
@@ -3,27 +3,23 @@ require 'rails_helper'
 RSpec.describe "Receptions", type: :feature do
   reception = FactoryBot.create(:reception)
 
-  # 以下のエラーが発生するため、一旦テストは飛ばす
-  # Failure/Error: <%= stylesheet_link_tag 'application', media: 'all', 'data-turbolinks-track': 'reload' %>
-  # ActionView::Template::Error:
-  #     Invalid CSS after "...ckground-image:": expected expression (e.g. 1px, bold), was "}"
-
-  xit '[new]にアクセス可' do
+  it '[new]にアクセス可' do
     visit new_reception_path
     expect(page).to have_current_path('/receptions/new')
   end
 
   xit 'post後記入項目が足りていれば、サクセスメッセージがでる' do
     visit new_reception_path
-    fill_in 'name', with: 'tanaka'
-    fill_in 'purpose', with: '1'
+    fill_in 'reception[name]', with: 'tanaka'
+    select 'reception[purpose]', from: '面談'
+    click_on '送信'
     expect(page).to have_content '完了'
   end
 
   xit 'post後記入項目が不足していたら、エラーメッセージが出る' do
     visit new_reception_path
-    fill_in 'name', with: ''
-    fill_in 'purpose', with: ''
+    fill_in 'reception[name]', with: ''
+    fill_in 'reception[purpose]', with: ''
     expect(page).to have_content '記入'
   end
 end
