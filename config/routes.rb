14f018828b41727f8d0ac8e8736b079cc237eeb6
yongju <yongjujiwork07@gmail.com>
2020-09-23 16:58:12 +0900

    sessions コントローラの作成とリソースの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   resources :receptions, only: [:new, :create, :index]
 end
