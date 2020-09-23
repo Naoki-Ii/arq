0f9cbaa6046e3761d57230560ef07d23327d1c16
yongju <yongjujiwork07@gmail.com>
2020-09-23 16:05:41 +0900

    passwordカラムを追加

--- app/models/room.rb
+++ app/models/room.rb
@@ -2,4 +2,5 @@ class Room < ApplicationRecord
   #VALID_NAME_REGEXは名前の命名制限-半角アルファベットの小文字のみ
   VALID_NAME_REGEX = /[a-z]/
   validates :name, presence: true, length: { maximum: 50 }, format: { with: VALID_NAME_REGEX }
+  has_secure_password
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_23_055058) do
+ActiveRecord::Schema.define(version: 2020_09_23_063609) do
 
   create_table "receptions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2020_09_23_055058) do
     t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
