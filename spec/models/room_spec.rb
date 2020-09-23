877a15b3af52a10e5cf8eb0a18d8a16c1180360d
yongju <yongjujiwork07@gmail.com>
2020-09-23 15:10:41 +0900

    最低限のmodelテストを作成

--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Room, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  room = FactoryBot.create(:room)
+
+  it 'roomインスタンスが有効' do
+    expect(room).to be_valid
+  end
+
+  it 'nameの値が１文字以上なければ無効' do
+    room.name = ''
+    expect(room).not_to be_valid
+  end
+
+  it 'nameが50文字以上は無効' do
+    room.name = 'a' * 51
+    expect(room).not_to be_valid
+  end
 end
