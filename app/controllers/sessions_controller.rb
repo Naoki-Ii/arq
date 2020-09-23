967f9db8b610fc47439deacf7a1314099a9172b5
yongju <yongjujiwork07@gmail.com>
2020-09-23 17:49:21 +0900

    login失敗の処理を追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,16 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    room = Room.find_by(name: params[:session][:name])
+    if room && room.authenticate(params[:session][:password])
+    else
+      flash[:alert] = '名前かパスワードが違います'
+      redirect_to '/login'
+    end
+  end
+
+  def destroy
+  end
 end
