a27b0bb0e531a655316757137b3c8de765643dac
yongju <yongjujiwork07@gmail.com>
2020-09-24 14:35:54 +0900

    current_roomを定義

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,6 +1,13 @@
 module SessionsHelper
-  #ブラウザ内の一時cookiesに暗号化済みのroomID
+  #ブラウザ内の一時cookiesに暗号化済みのroomIDを格納する
   def log_in(room)
     session[:room_id] = room.id
+  end
+
+  # ログインしているユーザーがいたら、ユーザーを代入する
+  def current_room
+    if session[:room_id]
+      @current_room ||= Room.find_by(id: session[:room_id])
+    end
   end
 end
