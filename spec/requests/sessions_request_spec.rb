ff5a155f607b704a14330ab2257c3868bc0ea4ef
yongju <yongjujiwork07@gmail.com>
2020-09-24 16:32:21 +0900

    login テストを追加

--- spec/requests/sessions_request_spec.rb
+++ spec/requests/sessions_request_spec.rb
@@ -1,12 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe "Sessions", type: :request do
+  let(:room) { FactoryBot.create(:room) }
 
-  describe "" do
-    it "returns http success" do
+  describe "ログイン機能" do
+    it "ログインページにアクセスできる" do
       get login_path
-      expect(response).to have_http_status(:success)
+      render_template login_path
+    end
+
+    it "ログインすると、ログアウトできる" do
+      get login_path
+      post login_path, params: {
+        session: {
+          name: room.name,
+          password: room.password
+        }
+      }
+      delete logout_path
+      render_template login_path
     end
   end
-
 end
